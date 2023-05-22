7fa6566f432c20bd035acfe329867af9bc24f517
TabrezMZ <tabrez.mansuri4u@gmail.com>
2023-05-22 06:07:14 +0530

    design change in Nav & Home

--- src/component/NavBar/NavBar.js
+++ src/component/NavBar/NavBar.js
@@ -1,53 +1,63 @@
-import './NavBar.css'
-import { Link } from 'react-router-dom';
-import { useProduct } from "../../context/ProductContext"
-import {useNavigate} from "react-router-dom"
+import "./NavBar.css";
+import { Link } from "react-router-dom";
+import { useProduct } from "../../context/ProductContext";
+import { useNavigate } from "react-router-dom";
 
 export const NavBar = () => {
-    const { productState,productDispatch } = useProduct()
-    const navigate = useNavigate();
-    const searchFilter = (e) => {
-        productDispatch({type : 'SEARCH_FILTER', payload : e.target.value})
-        navigate("/products")
-    }
-    return (
-        <div className='nav-header'>
-            <ul className='navbar'>
-                <div className='navbar-main'>
-                    <div className='navbar-left'>
-                        <Link className='logo-link' to='/'>BookClub</Link>
-                    </div>
-                    <div className='search-container'>
-                        <i className="fa fa-search" aria-hidden="true"></i>
-                        <input type='search' name='search' autoComplete='false' onChange={(e)=>searchFilter(e)} placeholder='search products' className='search-bar' />
-                    </div>
-                    <ul className='navbar-right'>
-                        <li className='nav-login'>
-                            <button className='login-btn'><Link to='/login'>Login</Link></button>
-                        </li>
-                        <Link to='/wishlist'>
-                        <li className='nav-wishlist'>
-                            <div className='icon wishlist-badge'>
-                                <i className="fa fa-heart" title="Wishlist"></i>
-                                <div className="notification-icon flex-center">
-                                    <span>()</span>
-                                </div>
-                            </div>
-                        </li>
-                        </Link>
-                        <Link to='/cart'>
-                        <li className='nav-cart'>
-                            <div className='icon cart-badge'>
-                                <i className="fa fa-shopping-cart" title="Cart"></i>
-                                <div className="notification-icon flex-center">
-                                    <span>()</span>
-                                </div>
-                            </div>
-                        </li>
-                        </Link>
-                    </ul>
+  const { productState, productDispatch } = useProduct();
+  const navigate = useNavigate();
+  const searchFilter = (e) => {
+    productDispatch({ type: "SEARCH_FILTER", payload: e.target.value });
+    navigate("/products");
+  };
+  return (
+    <div className="nav-header">
+      <ul className="navbar">
+        <div className="navbar-main">
+          <div className="navbar-left">
+            <Link to="/">BookClub</Link>
+          </div>
+          <div className="search-container">
+            <i className="fa fa-search" aria-hidden="true"></i>
+            <input
+              type="search"
+              name="search"
+              autoComplete="false"
+              onChange={(e) => searchFilter(e)}
+              placeholder="search products"
+              className="search-bar"
+            />
+          </div>
+          <ul className="navbar-right">
+            <li>
+              <div
+                className="icon cart-badge"
+                onClick={() => navigate("/wishlist")}
+              >
+                <i className="fa fa-heart" title="Wishlist"></i>
+                <div className="notification-icon flex-center">
+                  {/* <span>{wishlist.length}</span> */}
                 </div>
-            </ul>
+              </div>
+            </li>
+            <li className="nav-cart" onClick={() => navigate("/cart")}>
+              <div className="icon cart-badge">
+                <i className="fa fa-shopping-cart" title="Cart"></i>
+                  <div className="notification-icon flex-center">
+                    {/* <span>{cart.length}</span> */}
+                  </div>
+              </div>
+            </li>
+            <li onClick={() => navigate("/user_profile")}>
+              <img
+                src="https://github.com/rutvikpumak/pustaka-ecom/blob/dev/images/logo.png?raw=true"
+                alt="login"
+                // title={token ? "User Profile" : "Sign In"}
+              />
+            </li>
+          </ul>
         </div>
-    )
+      </ul>
+    </div>
+  );
 };
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import './Home.css'
 import axios from 'axios';
-import {useNavigate} from "react-router-dom"
+import {useNavigate,Link} from "react-router-dom"
 import { useProduct } from "../../context/ProductContext"
 
 export const Home = () => {
@@ -24,33 +24,94 @@ export const Home = () => {
     }
 
     return (
-        <div className='home-container'>
-            <div className='container-ct'>
-                <div className='category-container'>
-                    <h2>Book Categories</h2>
-                    <p>Here you explore books by categories</p>
+        <>
+        <div className="home-container">
+          <div className="home-img-container">
+            <div className="bg-img-container"></div>
+            <div className="home-page-text">
+              <div className="main-text">
+                <h4>
+                  Welcome to <span className="title">Pustaka</span>,
+                </h4>
+                <div>
+                  <h1 className="main-text-title">For All Your</h1>
+                  <h1 className="main-text-title">Reading Needs</h1>
                 </div>
-                <div className='category-list'>
-                    {
-                        categories && categories.map(({ _id, categoryName, description }) => {
-                            return (
-                                <div className='category-box' key={_id} onClick={() => categoriesHandlre(categoryName)}>
-                                    <div className='cat-details'>
-                                        <h4>{categoryName}</h4>
-                                        <p>{description}</p>
-                                    </div>
-                                </div>
-                            )
-                        })
-                    }
-                </div>
+                <Link to="/product">
+                  <button className="link-btn shop-now-btn">SHOP NOW</button>
+                </Link>
+              </div>
             </div>
-            <div className='image-container'>
-                 <img src='https://images.unsplash.com/photo-1589998059171-988d887df646?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80'/>
+            <div className="overlay"></div>
+          </div>
+  
+          <div className="category-container flex-center">
+            <div className="container">
+              <div className="category-heading text-center">
+                <h2>Featured Book Categories</h2>
+                <p className="paragraph-md">
+                  There are many categories of books available at Pustaka. Choose your favorite one
+                  now.
+                </p>
+              </div>
+              <div className="category-row">
+                {categories &&
+                  categories.map(({ _id, categoryName, description }) => {
+                    return (
+                      <div className="box" key={_id} onClick={() => categoriesHandlre(categoryName)}>
+                        <div className="detail-box text-center">
+                          <h4>{categoryName}</h4>
+                          <p className="paragraph-sm">{description}</p>
+                        </div>
+                      </div>
+                    );
+                  })}
+              </div>
             </div>
-            <div>
-                <h2>Book List</h2>
-            </div>
+          </div>
         </div>
+        <footer className="footer footer-mn">
+          <section className="footer-mn-lt">
+            <h2>Pustaka</h2>
+            <p className=" hm-page-paragraph">
+              Fill your house with stacks of books, in all the crannies and all the nooks.
+            </p>
+            <p>Privacy Policy</p>
+            <p>Terms of Use</p>
+            <p className="paragraph-sm">© 2022 Pustaka</p>
+          </section>
+          <section className="footer-mn-rt">
+            <ul>
+              <li>Connect</li>
+              <li>
+                <a href="https://github.com/rutvikpumak" target="_blank" className="github-logo">
+                  GitHub
+                </a>
+              </li>
+              <li>
+                <a href="https://twitter.com/rutvikumak13" target="_blank" className="twitter">
+                  Twitter
+                </a>
+              </li>
+              <li>
+                <a href="https://linkedin.com/in/rutvikumak" target="_blank" className="linkedin">
+                  LinkedIn
+                </a>
+              </li>
+            </ul>
+          </section>
+          <section className="footer-mn-rt">
+            <ul>
+              <li>Resources</li>
+              <Link to="/signup">
+                <li>Sign Up</li>
+              </Link>
+              <Link to="/login">
+                <li>Sign In</li>
+              </Link>
+            </ul>
+          </section>
+        </footer>
+      </>
     )
 }
